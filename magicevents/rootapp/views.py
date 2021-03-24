f969309ee77b0da0fc22b57893542c043a99c24b
Jakub Cichalewski <jcichalewski@gmail.com>
2021-03-24 17:35:27 +0100

    Shifted some event unregistration logic to models.py

--- magicevents/rootapp/views.py
+++ magicevents/rootapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
 
 from .forms import RegistrationForm, EventUnregisterForm
 from .models import EventRegistration, Event
@@ -14,7 +15,8 @@ def register(request):
         return render(
             request, 'users/register.html',
             {'form': RegistrationForm}
-        )
+            )
+
     elif request.method == 'POST':
         form = RegistrationForm(request.POST)
 
@@ -44,12 +46,8 @@ def event_register(request):
     event = Event.objects.get(pk=request.POST['pk'])
     user = request.user
 
-    already_registered = \
-        EventRegistration.objects.filter(
-            event=event, user=user).count() != 0
-
-    if not already_registered:
-        # TODO: move the assignment of the unregistration code to models (?)
+    if not EventRegistration.already_registered(event, user):
+        # TODO: move the assignment of the unregistration code to models.py
         #       ensure uniqueness of the code for the user
         unregister_code = randint(111111, 999999)
         event.add_atendee(user=user, code=unregister_code)
@@ -60,45 +58,20 @@ def event_register(request):
 
     return events(request)
 
-# TODO: move some of this logic to model (?), some to form validation
-def get_message_from_code(code: str, user):
-    if not (code.isdigit() and len(code) == 6):
-        return 'Your code should be a six digit number'
-
-    code = int(code)
-
-    matched_count = EventRegistration.objects.filter(user=user,
-                                                registration_code=code).count()
-
-    if matched_count == 0:
-        return 'Your code does not match any registrations.'
-    elif matched_count > 1:
-        return 'Uncanny.'
-
-    # code is matched.
-    event = EventRegistration.objects.get(user=user,
-                                          registration_code=code).event
-
-    if event.get_duration() > 2:
-        return 'You cannot cancel your reservation\
-                for an event that is longer than two days.'
-    if event.get_days_to_start() < 2:
-        return 'You cannot cancel your reservation\
-                later than two days before the start of an event.'
-
-    event.remove_atendee(user, code)
-
-
-# TODO: refactor with django.contrib.messages
 @login_required
 def event_unregister(request):
     if request.method != 'POST': return redirect('start')
 
-    error_message = get_message_from_code(request.POST['code'], request.user)
-    if error_message != None:
-        messages.error(request, error_message, extra_tags='unregister')
+    user = request.user
+    code = request.POST['code']
+
+    try:
+        Event.remove_atendee_from_code(user, code)
+    except ValidationError as error:
+        messages.error(request, error.message, extra_tags='unregister')
     else:
         messages.success(request,
-                         'You have been succesfully unregistered.',
+                         'You have been successfully unregistered.',
                          extra_tags='unregister')
+
     return start(request)
